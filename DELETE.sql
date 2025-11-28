97f655c832ee49aa6e8ab2cb16e57d4a6567520d
gustavoalveslopes17 <gustavoeletromig@hotmail.com>
2025-11-28 14:43:55 -0300

    Delete DELETE.sql

--- DELETE.sql
+++
@@ -1,8 +0,0 @@
--- Remover reserva cancelada
-DELETE FROM Reserva WHERE status = 'cancelada';
-
--- Remover usuário visitante
-DELETE FROM Usuario WHERE tipo = 'visitante';
-
--- Remover exemplar danificado
-DELETE FROM Exemplar WHERE disponibilidade = 'manutencao';
